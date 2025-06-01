cd3c30bc043aaa7ec553a645bff23653553efcf0
Tanushka Sharma <tanushkas723@gmail.com>
2025-06-01 23:48:22 +0530

    css

--- src/components/FilterComponent/FilterComponent.jsx
+++ src/components/FilterComponent/FilterComponent.jsx
@@ -1,19 +1,9 @@
 import React from 'react';
 import './FilterComponent.css';
 
-const FilterComponent = ({ semester, branchSearch, nameSearch, onSemesterChange, onBranchSearchChange, onNameSearchChange }) => {
+const FilterComponent = ({ branchSearch, nameSearch, onBranchSearchChange, onNameSearchChange }) => {
   return (
     <div className="filter-container">
-      <div className="filter-item">
-        <label>Filter by Semester:</label>
-        <select value={semester} onChange={(e) => onSemesterChange(e.target.value)}>
-          <option value="">All</option>
-          {[...Array(8)].map((_, i) => (
-            <option key={i + 1} value={i + 1}>{i + 1}</option>
-          ))}
-        </select>
-      </div>
-
       <div className="filter-item">
         <label>Search by Branch:</label>
         <input 
